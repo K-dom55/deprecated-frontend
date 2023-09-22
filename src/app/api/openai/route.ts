351da5ebff25689ba29285c16729179968531916
Jay Lee <jay.lee@negocio.co.kr>
2023-09-23 04:30:40 +0900

    feat: openai api

---
+++ src/app/api/openai/route.ts
@@ -0,0 +1,35 @@
+import Error from "next/error";
+import { NextRequest, NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+});
+
+export async function POST(req: NextRequest) {
+  const { answer, customQ } = (await req.json()) as {
+    answer: string;
+    customQ: string;
+  };
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: customQ,
+      },
+      {
+        role: "user",
+        content: `${answer}`,
+      },
+    ],
+  });
+
+  if (!response.choices) return;
+
+  return NextResponse.json({
+    success: true,
+    message: response.choices,
+  });
+}
