0ea1cbbfaf60562ce5c3f64ced2ba4965a9a01a3
Jay Lee <jay.lee@negocio.co.kr>
2023-09-23 04:30:29 +0900

    feat: 프로젝트 세팅

---
+++ src/app/forms/page.tsx
@@ -0,0 +1,12 @@
+import FormsTemplate from "@/components/FormsTemplate";
+import { css } from "@emotion/react";
+
+import Image from "next/image";
+
+export default function Forms() {
+  return (
+    <>
+      <FormsTemplate />
+    </>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <h1>최애의 효능</h1>
+      <Link href="forms">작성하기</Link>
+    </>
+  );
+}
