8661a72b6e56a96c0d8e9270f18ee71c0fcb2363
dolphinlmg <dolphinlmg@naver.com>
2023-09-23 11:25:29 +0900

    feat: theme 선언

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  gray900: '#1d1d1d',
+  gray800: '#3c3c3c',
+  gray700: '#5b5b5b',
+  gray600: '#6e6e6e',
+  gray500: '#979797',
+  gray400: '#b7b7b7',
+  gray300: '#dadada',
+  gray200: '#eaeaea',
+  gray100: '#f3f3f3',
+  gray50: '#f9f9f9',
+  primaryBlack: '#1d1d1d',
+  secondaryNeonGreen: '#6568eb',
+  background: '#f9f9f9',
+  aggroFont: '@font',
+};
